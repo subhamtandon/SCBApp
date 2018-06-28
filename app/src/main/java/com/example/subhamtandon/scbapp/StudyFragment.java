65c5c43346a5ef3dc80df3906ad4e94c26e118bb
subhamtandon <subhamtandon98@gmail.com>
2018-06-28 14:26:33 +0530

    professionals spinner added

--- app/src/main/java/com/example/subhamtandon/scbapp/StudyFragment.java
+++ app/src/main/java/com/example/subhamtandon/scbapp/StudyFragment.java
@@ -1,16 +1,24 @@
 package com.example.subhamtandon.scbapp;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 /**
@@ -30,15 +38,76 @@ public class StudyFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_study, container, false);
 
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+                View mView = getLayoutInflater().inflate(R.layout.activity_professionals_spinner, null);
+
+                builder.setTitle("Choose your Professional");
+
+                final Spinner mSpinner = (Spinner) mView.findViewById(R.id.spinner);
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                        android.R.layout.simple_spinner_item,
+                        getResources().getStringArray(R.array.professionalsList));
+
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                mSpinner.setAdapter(adapter);
+
+                mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        final Intent intent;
+                        switch (position){
+                            case 1:
+                                intent = new Intent(getContext(), Professional1stActivity.class);
+                                startActivity(intent);
+                                break;
+                            case 2:
+                                intent = new Intent(getContext(), Professional2ndActivity.class);
+                                startActivity(intent);
+                                break;
+                            case 3:
+                                intent = new Intent(getContext(), Professional3rdActivity.class);
+                                startActivity(intent);
+                                break;
+                            case 4:
+                                intent = new Intent(getContext(), Professional4thActivity.class);
+                                startActivity(intent);
+                                break;
+                            default:
+                                Toast.makeText(getContext(), "Choose Your Professional", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                        Toast.makeText(getContext(), "Choose Your Professional", Toast.LENGTH_SHORT).show();
+
+                        }
+                });
+
+                builder.setView(mView);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
         //((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Study Section");
         //((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        Professional1stCard =(CardView)view.findViewById(R.id.Professional1stCard);
-        Professional2ndCard=(CardView)view.findViewById(R.id.Professional2ndCard);
-        Professional3rdCard=(CardView)view.findViewById(R.id.Professional3rdCard);
-        Professional4thCard=(CardView)view.findViewById(R.id.Professional4thCard);
+        //Professional1stCard =(CardView)view.findViewById(R.id.Professional1stCard);
+        //Professional2ndCard=(CardView)view.findViewById(R.id.Professional2ndCard);
+        //Professional3rdCard=(CardView)view.findViewById(R.id.Professional3rdCard);
+        //Professional4thCard=(CardView)view.findViewById(R.id.Professional4thCard);
 
-        Professional1stCard.setOnClickListener(new View.OnClickListener() {
+        /*Professional1stCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO: Link new fragment
@@ -69,7 +138,7 @@ public class StudyFragment extends Fragment {
                 startActivity(new Intent(getContext(),Professional4thActivity.class));
                 //Toast.makeText(getActivity(), "Will be available soon", Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
 
         return view;
     }
