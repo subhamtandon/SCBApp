d66a9a693fc72996a49265fd2edf2f3311603904
Sourav Kumar Behera <beherasourav29@gmail.com>
2018-06-28 17:55:57 +0530

    Floating button added in 1st professional

--- app/src/main/java/com/example/subhamtandon/scbapp/Professional1stActivity.java
+++ app/src/main/java/com/example/subhamtandon/scbapp/Professional1stActivity.java
@@ -14,6 +14,8 @@ import android.widget.Toast;
 
 public class Professional1stActivity extends AppCompatActivity {
 
+    FloatingActionButton changeProfession;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,63 @@ public class Professional1stActivity extends AppCompatActivity {
 
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("1st Professional");
+
+        changeProfession = (FloatingActionButton)findViewById(R.id.changeProfession);
+        changeProfession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Professional1stActivity.this);
+
+                View mView = getLayoutInflater().inflate(R.layout.activity_professionals_spinner, null);
+
+                builder.setTitle("Choose your Professional");
+
+                final Spinner mSpinner = (Spinner) mView.findViewById(R.id.spinner);
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(Professional1stActivity.this,
+                        android.R.layout.simple_spinner_item,
+                        getResources().getStringArray(R.array.from1stProfessional));
+
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+                mSpinner.setAdapter(adapter);
+
+                mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        final Intent intent;
+                        switch (position){
+                            case 1:
+                                intent = new Intent(Professional1stActivity.this, Professional2ndActivity.class);
+                                startActivity(intent);
+                                break;
+                            case 2:
+                                intent = new Intent(Professional1stActivity.this, Professional3rdActivity.class);
+                                startActivity(intent);
+                                break;
+                            case 3:
+                                intent = new Intent(Professional1stActivity.this, Professional4thActivity.class);
+                                startActivity(intent);
+                                break;
+                            default:
+                                Toast.makeText(Professional1stActivity.this, "Choose Your Professional", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+
+                        Toast.makeText(Professional1stActivity.this, "Choose Your Professional", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                builder.setView(mView);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
 
     }
 }
