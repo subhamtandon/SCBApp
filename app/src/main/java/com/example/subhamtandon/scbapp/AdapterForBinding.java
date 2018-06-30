c95ec4863c9ec47f4faafce83ea9acc355bbb6d9
Sourav Kumar Behera <beherasourav29@gmail.com>
2018-06-30 17:51:10 +0530

    RecyclerView Clickable

--- app/src/main/java/com/example/subhamtandon/scbapp/AdapterForBinding.java
+++ app/src/main/java/com/example/subhamtandon/scbapp/AdapterForBinding.java
@@ -6,7 +6,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -30,10 +32,17 @@ public class AdapterForBinding extends RecyclerView.Adapter<AdapterForBinding.Vi
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-        ListItemChapters listItemChapter = listItemChapters.get(position);
+        final ListItemChapters listItemChapter = listItemChapters.get(position);
 
         holder.chapterHead.setText(listItemChapter.getChapterHead());
         holder.chapterDesc.setText(listItemChapter.getChapterDesc());
+
+        holder.linearLayoutChapters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Work is going on on "+listItemChapter.getChapterHead(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -44,12 +53,14 @@ public class AdapterForBinding extends RecyclerView.Adapter<AdapterForBinding.Vi
     public class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView chapterHead, chapterDesc;
+        LinearLayout linearLayoutChapters;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
             chapterHead = (TextView)itemView.findViewById(R.id.textViewChapterName);
             chapterDesc = (TextView)itemView.findViewById(R.id.textViewChapterDesc);
+            linearLayoutChapters = (LinearLayout) itemView.findViewById(R.id.linearLayoutChapters);
         }
     }
 }
