34ec25763d0a8a4cba8432f6a56b0672b76db246
Sourav Kumar Behera <beherasourav29@gmail.com>
2018-07-05 19:48:42 +0530

    Question layout modified

--- app/src/main/java/com/example/subhamtandon/scbapp/EasyQuestionSetActivity.java
+++ app/src/main/java/com/example/subhamtandon/scbapp/EasyQuestionSetActivity.java
@@ -1,9 +1,16 @@
 package com.example.subhamtandon.scbapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EasyQuestionSetActivity extends AppCompatActivity {
+
+    public void setOneClicked(View view){
+        Intent i = new Intent(EasyQuestionSetActivity.this, QuestionsActivity.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
