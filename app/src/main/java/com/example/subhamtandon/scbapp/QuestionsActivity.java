bfbc5a006a5da61cc4b196b66a71358c2a8a1f0c
subhamtandon <subhamtandon98@gmail.com>
2018-07-05 20:28:03 +0530

    exit dialog added in questions activity

--- app/src/main/java/com/example/subhamtandon/scbapp/QuestionsActivity.java
+++ app/src/main/java/com/example/subhamtandon/scbapp/QuestionsActivity.java
@@ -1,7 +1,11 @@
 package com.example.subhamtandon.scbapp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class QuestionsActivity extends AppCompatActivity {
 
@@ -10,4 +14,39 @@ public class QuestionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
     }
+
+    public void exitQuestions(View view){
+
+        exitDialog();
+    }
+    @Override
+    public void onBackPressed(){
+
+        Log.d("CDA", "onBackPressed Called");
+        exitDialog();
+
+    }
+
+    public void exitDialog(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
 }
