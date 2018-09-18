db2babf6371e515d0069443c67cf266ae4291785
subhamtandon <subhamtandon98@gmail.com>
2018-09-18 21:46:22 +0530

    data added in AdminDoctorsDetails

--- app/src/main/java/com/example/subhamtandon/scbapp/AdminDoctorsDetails.java
+++ app/src/main/java/com/example/subhamtandon/scbapp/AdminDoctorsDetails.java
@@ -1,11 +1,24 @@
 package com.example.subhamtandon.scbapp;
 
+import android.provider.ContactsContract;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AdminDoctorsDetails extends AppCompatActivity {
+
+    Button addNewAssistantProfessor, addNewAssociateProfessor, addNewHodName;
+    ImageView editHodName;
+    DatabaseReference databaseReferenceHod, databaseReferenceAssistantProfessor, databaseReferenceAssociateProfessor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,106 @@ public class AdminDoctorsDetails extends AppCompatActivity {
 
         String departmentName = getIntent().getStringExtra("DEPARTMENT NAME");
         Toast.makeText(this, departmentName, Toast.LENGTH_SHORT).show();
+
+        addNewAssistantProfessor = findViewById(R.id.addNewAssistantProfessor);
+        addNewAssociateProfessor = findViewById(R.id.addNewAssociateProfessor);
+        addNewHodName = findViewById(R.id.addNewHodName);
+        editHodName = findViewById(R.id.editHodName);
+
+        databaseReferenceHod = FirebaseDatabase.getInstance().getReference("App").child("Departments").child(departmentName).child("HOD");
+        databaseReferenceAssistantProfessor = FirebaseDatabase.getInstance().getReference("App").child("Departments").child(departmentName).child("Assistant Professor");
+        databaseReferenceAssociateProfessor = FirebaseDatabase.getInstance().getReference("App").child("Departments").child(departmentName).child("Associate Professor");
+
+        addNewHodName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(AdminDoctorsDetails.this);
+                View mView = getLayoutInflater().inflate(R.layout.enter_doctors_name, null);
+
+                final TextInputEditText textInputEditTextName = mView.findViewById(R.id.textInputEditTextName);
+                Button enterNameButton = mView.findViewById(R.id.enterNameButton);
+
+                mBuilder.setCancelable(false);
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+
+                enterNameButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!textInputEditTextName.getText().toString().isEmpty()){
+                            Toast.makeText(AdminDoctorsDetails.this, "Name added successfully", Toast.LENGTH_SHORT).show();
+                            final String id = databaseReferenceHod.push().getKey();
+                            databaseReferenceHod.child(id).setValue(textInputEditTextName.getText().toString());
+                            dialog.dismiss();
+
+                        }else {
+                            Toast.makeText(AdminDoctorsDetails.this, "Please fill the name", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        addNewAssistantProfessor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(AdminDoctorsDetails.this);
+                View mView = getLayoutInflater().inflate(R.layout.enter_doctors_name, null);
+
+                final TextInputEditText textInputEditTextName = mView.findViewById(R.id.textInputEditTextName);
+                Button enterNameButton = mView.findViewById(R.id.enterNameButton);
+
+                mBuilder.setCancelable(false);
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+
+                enterNameButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!textInputEditTextName.getText().toString().isEmpty()){
+                            Toast.makeText(AdminDoctorsDetails.this, "Name added successfully", Toast.LENGTH_SHORT).show();
+                            final String id = databaseReferenceAssistantProfessor.push().getKey();
+                            databaseReferenceAssistantProfessor.child(id).setValue(textInputEditTextName.getText().toString());
+                            dialog.dismiss();
+                        }else {
+                            Toast.makeText(AdminDoctorsDetails.this, "Please fill the name", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        addNewAssociateProfessor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(AdminDoctorsDetails.this);
+                View mView = getLayoutInflater().inflate(R.layout.enter_doctors_name, null);
+
+                final TextInputEditText textInputEditTextName = mView.findViewById(R.id.textInputEditTextName);
+                Button enterNameButton = mView.findViewById(R.id.enterNameButton);
+
+                mBuilder.setCancelable(false);
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+
+                enterNameButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!textInputEditTextName.getText().toString().isEmpty()){
+                            Toast.makeText(AdminDoctorsDetails.this, "Name added successfully", Toast.LENGTH_SHORT).show();
+                            final String id = databaseReferenceAssociateProfessor.push().getKey();
+                            databaseReferenceAssociateProfessor.child(id).setValue(textInputEditTextName.getText().toString());
+                            dialog.dismiss();
+                        }else {
+                            Toast.makeText(AdminDoctorsDetails.this, "Please fill the name", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 
     @Override
